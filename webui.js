d14a8e244ece3b46baf794fffa33c8b70db6e943
Ryan Quinlan <ryan.quinlan@gmail.com>
2021-05-23 23:27:40 -0400

    kiss

--- webui.js
+++ webui.js
@@ -54,3 +54,14 @@ function initialise(node, conference, userbw, name, userpin) {
 
     rtc.makeCall(node, conference, name, bandwidth);
 }
+
+
+var chatDiv = document.getElementById("chat-box");
+
+rtc.onChatMessage = function(message) {
+  console.log(message)
+  var newChat = document.createElement('div');
+  newChat.className = 'container'
+  newChat.innerHTML = `<p>${ message.payload }</p><span class="time-right">${ Date().toLocaleString() }</span>`
+  chatDiv.appendChild(newChat)
+}
